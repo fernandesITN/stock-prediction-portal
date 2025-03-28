8380ffe8a8b0422f390f4da24b6d3f82a1257568
fernandesITN <fernandesitsnext@gmail.com>
2025-03-28 09:52:09 -0300

    up

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import './assets/css/style.css'
+import Header from './assets/components/Header'
+import Main from './assets/components/Main'
+import Footer from './assets/components/Footer'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Main />
+      <Footer/>
     </>
   )
 }
